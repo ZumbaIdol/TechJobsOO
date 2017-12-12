c6b3edc89666fa48eb783eb1062c309ca1132674
David A. Foote <david.a.foote@gmail.com>
2017-12-11 19:58:48 -0600

    Made the code DRY by commenting out common code that was moved to base class.

--- src/TechJobs/ViewModels/JobFieldsViewModel.cs
+++ src/TechJobs/ViewModels/JobFieldsViewModel.cs
@@ -10,16 +10,16 @@ namespace TechJobs.ViewModels
         // to BaseViewModel
 
         // The current column
-        public JobFieldType Column { get; set; }
+        //public JobFieldType Column { get; set; }
 
         // All fields in the given column
         public IEnumerable<JobField> Fields { get; set; }
 
         // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
+        //public List<JobFieldType> Columns { get; set; }
 
         // View title
-        public string Title { get; set; } = "";
+       // public string Title { get; set; } = "";
 
         public JobFieldsViewModel()
         {
--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -14,17 +14,17 @@ namespace TechJobs.ViewModels
         public List<Job> Jobs { get; set; }
 
         // The column to search, defaults to all
-        public JobFieldType Column { get; set; } = JobFieldType.All;
+       // public JobFieldType Column { get; set; } = JobFieldType.All;
 
         // The search value
         [Display(Name = "Keyword:")]
         public string Value { get; set; } = "";
 
         // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
+       // public List<JobFieldType> Columns { get; set; }
 
         // View title
-        public string Title { get; set; } = "";
+        //public string Title { get; set; } = "";
 
         public SearchJobsViewModel()
         {
